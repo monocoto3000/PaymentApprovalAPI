b2c0f045d9b466d44e8830ef6053b75b8e90760d
monocoto3000 <223238@ids.upchiapas.edu.mx>
2024-03-03 23:36:37 -0600

    AMQP update

--- src/payment/infraestructure/dependencies.ts
+++ src/payment/infraestructure/dependencies.ts
@@ -1,12 +1,12 @@
-import { deliverMessagetoQueue } from "../../shared/broker/application/deliverMessage";
 import { CreatePaymentUseCase } from "../application/createPaymentUseCase";
+import { deliverMessagetoQueue } from "../../shared/broker/application/deliverMessage";
 import { SocketIO } from "../../shared/socket/infraestructure/SocketIO";
 import { deliverDatatoClient } from "../../shared/socket/application/deliverDatatoClientUseCase";
 import { CreatePaymentController } from "./controllers/CreatePaymentController";
 import { Amqplib } from "../../shared/broker/infraestructure/Amqplib";
 
 // Ports
-const amqplLib = new Amqplib("amqp://localhost");
+const amqplLib = new Amqplib("amqp://52.6.228.180/");
 const socketIo = new SocketIO("http://localhost:4000");
 
 //Deliver data
